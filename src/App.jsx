34eace863a8139c93d73b678f2e6d17a9a2bfd0e
CHABANI Walid <chabaniw20@gmail.com>
2024-01-27 14:24:18 +0100

    added van route page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link} from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from "./pages/Vans"
 import './server'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
+        <Route path="/vans" element={<Vans/>} />
       </Routes>
     </BrowserRouter>
   )
