2fa4d7a9744d1c8540aa5b454140bbbc243d5784
CHABANI Walid <chabaniw20@gmail.com>
2024-01-27 13:58:17 +0100

    set the home and about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <header>
+        <Link to='/' className="site-logo">#VANLIFE</Link>
+        <nav>
+          <Link to='/vans'>Vans</Link>
+          <Link to='/about'>About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
