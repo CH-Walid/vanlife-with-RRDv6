77658b73abbc2d95f4dcb7b338799f9b54f52ca9
CHABANI Walid <chabaniw20@gmail.com>
2024-02-04 00:52:45 +0100

    Setting up the VanDetail Page

---
+++ src/pages/VanDetail.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const VanDetail = () => {
+  const { id } = useParams();
+  const [van, setVan] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/vans/${id}`)
+      .then((res) => res.json())
+      .then((data) => setVan(data.vans));
+  }, [id]);
+
+  return (
+    <div className="van-detail-container">
+      {van ? (
+        <div className="van-detail">
+          <img src={van.imageUrl} alt={van.name} />
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <h1>{van.name}</h1>
+          <p className="van-price">
+            <span>${van.price}</span>/day
+          </p>
+          <p>{van.description}</p>
+          <button className="link-button">Rent this van</button>
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
+};
+export default VanDetail;
