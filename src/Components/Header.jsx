ef1f4d483c289228590dc0516d52c499fadb924a
CHABANI Walid <chabaniw20@gmail.com>
2024-02-04 13:21:27 +0100

    Active NavLink styling for header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,33 @@
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+
+const navLinks = [
+  { path: "/host", text: "Host" },
+  { path: "/vans", text: "Vans" },
+  { path: "/about", text: "About" },
+];
 
 const Header = () => {
+  const active = {
+    fontWeigt: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+
   return (
     <header>
       <Link to="/" className="site-logo">
         #VANLIFE
       </Link>
       <nav>
-        <Link to="/host">Host</Link>
-        <Link to="/vans">Vans</Link>
-        <Link to="/about">About</Link>
+        {navLinks.map(({ path, text }, index) => (
+          <NavLink
+            key={index}
+            style={({ isActive }) => (isActive ? active : null)}
+            to={path}
+          >
+            {text}
+          </NavLink>
+        ))}
       </nav>
     </header>
   );
